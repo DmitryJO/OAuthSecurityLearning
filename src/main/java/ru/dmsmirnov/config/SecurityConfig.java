87b7a845697bb8f573844efc09a737fcedef1ceb
dmsmirnov <dmsmirnov@mmtr.ru>
2025-05-13 19:05:49 +0300

    add client

--- src/main/java/ru/dmsmirnov/config/SecurityConfig.java
+++ src/main/java/ru/dmsmirnov/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package ru.dmsmirnov.config;
 
-import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,12 +21,15 @@ public class SecurityConfig {
         http.oauth2ResourceServer(oauth2 -> oauth2
                 .authenticationManagerResolver(resolver));
 
-        http.authorizeHttpRequests(
-                c -> c.requestMatchers("/hello").permitAll()
-                        .requestMatchers("/manager").hasAnyAuthority("manager", "admin")
-                        .requestMatchers("/superuser").hasAuthority("admin")
-                        .anyRequest().authenticated()
-        );
+        http
+                .oauth2Login(c -> c.defaultSuccessUrl("/hello"))
+                .authorizeHttpRequests(
+                        c -> c
+                                .requestMatchers("/hello").permitAll()
+                                .requestMatchers("/manager").hasAnyAuthority("manager", "admin")
+                                .requestMatchers("/superuser").hasAuthority("admin")
+                                .anyRequest().authenticated()
+                );
 
         return http.build();
     }
